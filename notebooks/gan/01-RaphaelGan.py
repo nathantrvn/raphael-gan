9fee04d711070d7f893f8fe7047af75b5bf538a1
nTrouvain <ntrouvain@ensc.fr>
2019-11-13 16:05:02 +0100

    Building Raphael judgement

--- notebooks/gan/01-RaphaelGan.py
+++ notebooks/gan/01-RaphaelGan.py
@@ -5,38 +5,44 @@
 # 
 # Unleashing the power of DCNN to create random Raphaëls.
 
-# In[1]:
+# In[26]:
 
 
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, SpatialDropout2D, Conv2DTranspose
+from tensorflow import keras
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, SpatialDropout2D, Conv2DTranspose, UpSampling2D
 from tensorflow.keras.layers import Dropout, Input, Dense, Flatten, Reshape, BatchNormalization
 from tensorflow.keras.layers import LeakyReLU
 from tensorflow.keras.models import Sequential, Model
 
+SAVE_PATH = "../../models"
 
-# In[2]:
+
+# In[68]:
 
 
 def build_generator(latent_size):
     
     generator = Sequential([
-        # (100)
-        Input(shape=(100,), name="LatentNoise"),
-        Dense(16*16*384, activation="relu"),
+        # (latent_size)
+        Input(shape=(latent_size,), name="LatentNoise"),
+        Dense(16*16*128, activation="relu"),
         # (16, 16, 384)
-        Reshape((16, 16, 384)),
+        Reshape((16, 16, 128)),
         # (32, 32, 192)
-        Conv2DTranspose(192, 5, strides=2, padding="same", activation="relu"),
-        BatchNormalization(),
+        UpSampling2D(),
+        Conv2DTranspose(64, 3, padding="same", activation="relu"),
+        BatchNormalization(momentum=0.8),
         # (64, 64, 96)
-        Conv2DTranspose(96, 5, strides=2, padding="same", activation="relu"),
-        BatchNormalization(),
+        UpSampling2D(),
+        Conv2DTranspose(32, 3, padding="same", activation="relu"),
+        BatchNormalization(momentum=0.8),
         # (128, 128, 3)
-        Conv2DTranspose(3, 5, strides=2, padding="same", activation="tanh")
-    ])
+        UpSampling2D(),
+        Conv2DTranspose(3, 3, padding="same", activation="tanh")
+    ], name="generator")
     
     return generator
 
@@ -45,28 +51,85 @@ def build_discriminator():
     discriminator = Sequential([
         # (128, 128, 3)
         Input(shape=(128, 128, 3), name="InputImages"),
-        Conv2D(64, 3, padding="same", strides=1),
-        MaxPooling2D(padding="same"),
-        LeakyReLu(0.2),
-        SpatialDropout2D(0.2),
+        Conv2D(32, 3, padding="same", kernel_initializer="he_normal"),
+        LeakyReLU(0.2),
+        Conv2D(32, 3, padding="same", kernel_initializer="he_normal"),
+        LeakyReLU(0.2),
+        MaxPooling2D(),
+        Dropout(0.2),
         
-        Conv2D(128, 3, padding="same", strides=1),
-        LeakyReLu(0.2),
-        SpatialDropout2D(0.2),
+        Conv2D(64, 3, padding="same", kernel_initializer="he_normal"),
+        LeakyReLU(0.2),
+        Conv2D(64, 3, padding="same", kernel_initializer="he_normal"),
+        LeakyReLU(0.2),
+        MaxPooling2D(),
+        Dropout(0.2),
         
-        Conv2D(256, 3, padding="same", strides=1),
-        LeakyReLu(0.2),
-        SpatialDropout2D(0.2),
+        Conv2D(128, 3, padding="same", kernel_initializer="he_normal"),
+        LeakyReLU(0.2),
+        Conv2D(128, 3, padding="same", kernel_initializer="he_normal"),
+        LeakyReLU(0.2),
+        MaxPooling2D(),
         
         Flatten(),
+        Dropout(0.2),
+        Dense(128, activation="relu"),
+        Dropout(0.2),
         Dense(2, activation="softmax")        
-    ])
+    ], name="discriminator")
     
     return discriminator
+
+
+# In[69]:
+
+
+generator = build_generator(100)
+
+
+# In[70]:
+
+
+discriminator = build_discriminator()
+
+
+# In[71]:
+
+
+discriminator.summary()
+
+
+# In[72]:
+
+
+generator.summary()
+
+
+# In[79]:
+
+
+def build_combined(discriminator, generator, latent_size):
+    noise = Input(shape=(latent_size, ), name="noise")
+    fake_raph = generator(noise)
+    discriminator.trainable = False
+    validation = discriminator(fake_raph)
+    return Model(noise, validation, name="combined")
+
+
+# In[80]:
+
+
+combined = build_combined(discriminator, generator, 100)
+
+
+# In[81]:
+
+
+combined.summary()
 
 
 # In[ ]:
 
 
-generator = build_generator(100)
+
 
